85b474c13ddbe1bc4161845de0a668679edca79c
Dario Vladovic <d.vladimyr@gmail.com>
2020-09-13 05:08:56 +0200

    Update test suite
    - testing invalid API key handling
    - testing captcha handling
    - adding accompanying `nock` fixtures

--- test.js
+++ test.js
@@ -4,7 +4,6 @@ const nock = require('nock');
 const path = require('path');
 const Scholar = require('.');
 
-nock.back.setMode('record');
 nock.back.fixtures = path.join(__dirname, '__nock_fixtures__');
 
 beforeAll(() => Scholar.init('dummy'));
@@ -15,6 +14,7 @@ test('search publications', async () => {
   const { nockDone } = await nock.back('pub-search.json');
   const query = '"A frequency-domain analysis of haptic gratings"';
   const pub = await Scholar.searchPub(query);
+  nockDone();
   expect(pub).toEqual(expect.objectContaining({
     title: 'A frequency-domain analysis of haptic gratings',
     authors: expect.arrayContaining([
@@ -23,13 +23,13 @@ test('search publications', async () => {
       expect.objectContaining({ id: 'OiVOAHMAAAAJ', name: 'HZ Tan' })
     ])
   }));
-  nockDone();
 }, 10000);
 
 test('fetch publication authors', async () => {
   const { nockDone } = await nock.back('pub-authors.json');
   const query = '"A frequency-domain analysis of haptic gratings"';
   const authors = await Scholar.getPubAuthors(query);
+  nockDone();
   expect(authors).toEqual(expect.arrayContaining([
     expect.objectContaining({
       id: '4bahYMkAAAAJ',
@@ -60,5 +60,28 @@ test('fetch publication authors', async () => {
       hindex: '49'
     })
   ]));
-  nockDone();
+}, 10000);
+
+test.only('handle invalid API key', async () => {
+  const { nockDone } = await nock.back('invalid-api-key.json');
+  const query = '"A frequency-domain analysis of haptic gratings"';
+  try {
+    await Scholar.searchPub(query);
+  } catch (error) {
+    nockDone();
+    expect(error).toBeInstanceOf(Scholar.ScholarlyError);
+    expect(error).toBeInstanceOf(Scholar.ProxyError);
+  }
+}, 10000);
+
+test('handle captcha', async () => {
+  const { nockDone } = await nock.back('captcha.json');
+  const query = '"A frequency-domain analysis of haptic gratings"';
+  try {
+    await Scholar.searchPub(query);
+  } catch (error) {
+    nockDone();
+    expect(error).toBeInstanceOf(Scholar.ScholarlyError);
+    expect(error).toBeInstanceOf(Scholar.CaptchaError);
+  }
 }, 10000);
